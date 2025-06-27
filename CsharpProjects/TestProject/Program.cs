7ee8bec73de621e24dc5622e4c1a65d8c0eee061
sebastian lapt <smoreira11@hotmail.com>
2025-06-27 11:47:03 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,18 +1,35 @@
 ﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-bool validEntry = false;
-Console.WriteLine("Enter a string containing at least three characters:");
-do
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
 {
-    readResult = Console.ReadLine();
-    if (readResult != null)
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
     {
-        if (readResult.Length >= 3)
-        {
-            validEntry = true;
-        }
-        else
-        {
-            Console.WriteLine("Your input is invalid, please try again.");
-        }
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
     }
-} while (validEntry == false);
+ 
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
